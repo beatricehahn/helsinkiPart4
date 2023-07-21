4f9ef90c2d7a99a860d1277ed3aa9fd9d63aeb82
Beatrice Hahn <beatricehahn7@gmail.com>
2023-07-20 17:46:16 -0700

    Isolate config as a separate module

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const app = require('./app') // load the express app from app.js
 const logger = require('./utils/logger') // info and error logging
+const config = require('./utils/config')
 
 app.listen(config.PORT, () => {
     logger.info(`Server is running on port ${config.PORT}`)
