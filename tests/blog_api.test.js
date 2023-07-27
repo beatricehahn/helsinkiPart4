472dbd8a47b4bcb59c12a9da91c82e4c5994331e
Beatrice Hahn <beatricehahn7@gmail.com>
2023-07-27 12:23:47 -0700

    Add GET request api test

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -1,6 +1,42 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app') // imports the express app
+const Blog = require('../models/blog.schema')
+
+const initialBlogs = [
+    {
+        title: "Canonical string reduction",
+        author: "Edsger W. Dijkstra",
+        likes: 12
+    },
+    {
+        title: "Giraffe",
+        author: "Kane Ellis",
+        likes: 1
+    },
+    {
+        title: "Cow sightings",
+        author: "Max Pallor",
+        likes: 10
+    },
+    {
+        title: "Bird eggs",
+        author: "Jane Goodwell",
+        likes: 9
+    }
+]
+// initialize database before every test
+beforeEach(async () => {
+    await Blog.deleteMany({})
+    let blogObject = new Blog(initialBlogs[0])
+    await blogObject.save()
+    blogObject = new Blog(initialBlogs[1])
+    await blogObject.save()
+    blogObject = new Blog(initialBlogs[2])
+    await blogObject.save()
+    blogObject = new Blog(initialBlogs[3])
+    await blogObject.save()
+})
 
 // create and save a superagent object
 const api = supertest(app)
